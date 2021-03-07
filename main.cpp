7a591f5a102c54c185ae7fcbec55608aec9e38f4
ShinjeongA <jeonga@khu.ac.kr>
2021-03-08 00:21:27 +0900

    나이 계산하기

--- main.cpp
+++ main.cpp
@@ -5,37 +5,39 @@ using namespace std;
 
 int main() {
 
-	ifstream fin;
+	//*** 형변환시 주의 사항***
+	//문자를 그대로 int 형으로 바로 형변환해버리면 그냥 아스키코드에 해당되는 값으로만 계산됨
+	//내가 원하는 숫자로 하려면 반드시 (int) 이렇게 붙인 거에다가 -48까지 해줘야함
 
-	fin.open("input.txt");
+	string number;
 
-	int min=101;
-	int max=1;
-	int num;
+	cin >> number;
 
-	int count;
 
-	fin >> count;
 
-	for (int i = 0; i < count; i++) {
-		fin >> num;
-		if (num < min) {
-			min = num;
-		}
-		if (num > max) {
-			max = num;
-		}
-		
+	int year;
+
+	if (((int)number[0]-48) == 0) {
+		year = 2000 + ((int)number[0]-48) * 10 +((int)number[1]-48);
+	}
+	else {
+		year=1900+((int)number[0]-48) * 10 + ((int)number[1]-48);
 	}
 
-
-	int res = max - min;
-
-	ofstream fout;
-	fout.open("output.txt");
-
-	fout << res << endl;
+	int age = (2019 - year) + 1;
 
 
+
+	int gender1 = ((int)number[7]-48);
+	string gender2;
+	if (gender1 == 1 || gender1 == 3) {
+		gender2 = "M";
+	}
+	else {
+		gender2 = "W";
+	}
+
+	cout << age<<" "<< gender2 << endl;
+	
 	
 }
