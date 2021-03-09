fe02d59a4cbd39bc52f6349cdaff327cba501936
ShinjeongA <jeonga@khu.ac.kr>
2021-03-09 22:29:15 +0900

    6.숫자추출

--- main.cpp
+++ main.cpp
@@ -9,35 +9,36 @@ int main() {
 	//문자를 그대로 int 형으로 바로 형변환해버리면 그냥 아스키코드에 해당되는 값으로만 계산됨
 	//내가 원하는 숫자로 하려면 반드시 (int) 이렇게 붙인 거에다가 -48까지 해줘야함
 
-	string number;
-
-	cin >> number;
-
-
-
-	int year;
-
-	if (((int)number[0]-48) == 0) {
-		year = 2000 + ((int)number[0]-48) * 10 +((int)number[1]-48);
-	}
-	else {
-		year=1900+((int)number[0]-48) * 10 + ((int)number[1]-48);
-	}
-
-	int age = (2019 - year) + 1;
-
-
-
-	int gender1 = ((int)number[7]-48);
-	string gender2;
-	if (gender1 == 1 || gender1 == 3) {
-		gender2 = "M";
-	}
-	else {
-		gender2 = "W";
-	}
-
-	cout << age<<" "<< gender2 << endl;
 	
-	
+	string input;
+	int res = 0;//최종 숫자
+	int count=0; // 약수의 개수
+
+
+	cin >> input;
+
+	int length = input.length();
+
+	for (int i = 0; i < length; i++) {
+
+		if (input[i] > 47 && input[i] < 58) {
+
+			res = res * 10 + (input[i] - 48); // 누적된 거에 *10을 해주는 거임!!
+		
+		}
+	}
+
+
+
+	for (int i = 1; i < res+1; i++) {
+
+		if (res % i == 0) {
+			count++;
+		}
+	}
+
+	cout << res << endl<<count;
+
+
+
 }
