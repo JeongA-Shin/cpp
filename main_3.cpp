dd2e63fa1a53b0371fa30cf549c5b5c6b5f5b244
ShinjeongA <jeonga@khu.ac.kr>
2021-05-17 23:03:05 +0900

    진약수의 합

---
+++ main_3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	int n;
+	cin >> n;
+
+
+	cout << "1";
+
+	int sum = 1; //1은 무조건 진약수이므로
+
+
+	for (int i = 2; i < n+1 ; i++) {
+		if (n % i == 0) {
+			if (i < n) {
+				cout <<"+"<< i;
+				sum += i;
+
+			}
+			else {
+				cout << "=";
+				break;
+			}
+		}
+
+	}
+
+	cout << sum;
+}
