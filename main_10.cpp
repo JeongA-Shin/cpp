040bb3052a463dc13aff6d2c083a73a483d0d453
ShinjeongA <jeonga@khu.ac.kr>
2021-05-20 13:59:36 +0900

    자릿수의 합

--- main_10.cpp
+++ main_10.cpp
@@ -3,122 +3,46 @@ using namespace std;
 
 
 
+
+//자릿수의 합을 출력함
 int digit_sum(int x) {
 
+	int sum = 0;
 
-	int res = 0;
-
-	while (x > 0) {
-
-		int tmp = 0;
-		tmp = x % 10; //일의 자리만 남음
-		res += tmp;
-		x = x / 10; //x는 한 자리 올라감
-
+	while (x >= 1) {
+		sum += x % 10;
+		x =x / 10;		
 	}
 
-	return res;
+	return sum;
 
 }
 
 int main() {
 
+	int N;
 
-	/* 내가 한 거 --> 너무 복잡함;;;
-	int n;
-	cin >> n;
+	cin >> N;
 
-	int** sum = new int*[2]; //[row] 부분// 가로칸이 크게 2개있음
 
-	for (int i = 0; i < 2; i++) {
-		sum[i] = new int[n]; //세로부분
+	int input;
+	int max = 0;
+	int res = 0;
+
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		if (digit_sum(input) > max) {
+			max = digit_sum(input); //자릿수의 합의 최대
+			res = input; // 그리고 해당 숫자
+		}
+		else if (digit_sum(input) == max) {
+			if (input > res) {
+				res = input;
+			}
+		}
 	}
-
-	int num;
-
-	for (int j = 0; j < n; j++) {
-
-		cin >> num;
-		sum[0][j] = num; // 제일 위 가로줄에는 입력한 값들이 들어간다.
-	}
-
-
-
 	
-	for (int j = 0; j < n; j++) {
-
-		int temp = digit_sum(sum[0][j]);
-		sum[1][j] = temp; // 두 번째 가로줄에는 각 칸마다 digit_sum을 해준 값들이 들어간다.
-	}
-
-
-	int pre = 0;
-	int post = 0;
-
-	int pre_sum = 0;
-	int post_sum = 0;
-
-
-	int result=0;
-
-	pre_sum = sum[1][0];
-	pre = 0;// 윗줄에서 칸 위치
-
-	for (int k = 1; k < n; k++) {
-		post_sum = sum[1][k];
-		post = k;
-
-		if (pre_sum > post_sum) {
-			result = sum[0][pre];
-
-		}else if (pre_sum < post_sum) {
-			result = sum[0][post];
-
-		}else {
-			if (sum[0][pre] > sum[0][k]) {
-				result = sum[0][pre];
-			}
-			else{
-				result = sum[0][post];
-			}
-		}
-
-		pre_sum = sum[1][pre];
-		pre = post;
-
-		
-	}
-
-
-	
-
-	cout << result << endl;
-
-	*/
-
-
-	int n, num, i, sum, max = -1234, res;
-
-	//max는 합을 보는 것
-	//res는 입력한 숫자를 저장한 것
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		sum = digit_sum(num);
-		if (sum > max) {
-			max = sum;
-			res = num;
-		}
-		else if (sum == max) {
-
-			if (num > res) {
-				res = num;
-			}
-		}
-	}
-
-
 	cout << res << endl;
+
 
 }
