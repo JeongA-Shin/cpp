244b5d5aec7fb810e9896fa893fdc883d97a2baa
ShinjeongA <jeonga@khu.ac.kr>
2021-05-27 15:39:48 +0900

    가위 바위 보

--- main_20.cpp
+++ main_20.cpp
@@ -26,6 +26,16 @@ int main() {
 		arr[2][j] = B;
 	}
 
+	//가위 바위 보 승부 판별 더 간단하게 하기
+	/*
+	for (i = 1; i <= n; i++) {
+		if (a[i] == b[i]) printf("D\n");  //1. 비기는 경우
+		else if (a[i] == 1 && b[i] == 3) printf("A\n"); //2. A가 이기는 경우
+		else if (a[i] == 2 && b[i] == 1) printf("A\n");
+		else if (a[i] == 3 && b[i] == 2) printf("A\n");
+		else printf("B\n"); //3. 그 외에는 B가 이김
+	}*/
+
 
 	for (int i = 0; i < N; i++) {
 		if (arr[1][i] == 1 && arr[2][i] == 2) {
