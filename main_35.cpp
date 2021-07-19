856a44a95aea9450ab6fbcbe3249f4fdd83edb8f
ShinjeongA <jeonga@khu.ac.kr>
2021-07-20 00:15:40 +0900

    inversion sequence - 삽입정렬의 활용

--- main_35.cpp
+++ main_35.cpp
@@ -31,3 +31,41 @@ int main() {
 		cout << *itr << " ";
 	}
 }
+
+/*
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+
+
+	vector<int> plus;
+	vector<int> minus;
+
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		if (num > 0) {
+			plus.push_back(num);
+		}
+		else {
+			minus.push_back(num);
+		}
+	}
+
+
+	minus.insert(minus.end(), plus.begin(), plus.end());
+
+	vector<int>::iterator itr;
+
+	for (itr = minus.begin(); itr != minus.end(); itr++) {
+		cout << *itr << " ";
+	}
+}
+
+*/
